a8495bd446960db9f00cc71ae9b28368f2b4cff0
[esekyi] <[sskert10@gmail.com]>
2024-07-12 05:44:22 +0000

    ignores

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""a Fabric script that generates a .tgz archive from the contents of the web_static"""
+"""a Fabric script that generates a .tgz archive
+from the contents of the web_static"""
 
 from fabric.api import local
 from datetime import datetime
