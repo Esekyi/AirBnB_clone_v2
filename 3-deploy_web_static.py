73b181189048367c81eafc7935fcbf0494cf7a11
[esekyi] <[sskert10@gmail.com]>
2024-07-15 07:37:47 +0000

    fixes

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -46,20 +46,21 @@ def do_deploy(archive_path):
 
     try:
         split_path = archive_path.split('/')
-        file_name = split_path[-1].split('.')[0]
+        filename = split_path[-1]
+        file_name = filename.split('.')[0]
         print("Executing task 'do_deploy'")
         put(archive_path, '/tmp/')
         run('mkdir -p /data/web_static/releases/{}'.format(file_name))
-        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'
-            .format(archive_path, file_name))
-        run('rm /tmp/{}'.format(archive_path))
+        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}'
+            .format(filename, file_name))
+        run('rm /tmp/{}'.format(filename))
         run("""mv /data/web_static/releases/{}/web_static/*
-              /data/web_static/releases/{}/"""
+             /data/web_static/releases/{}/"""
             .format(file_name, file_name))
         run('rm -rf /data/web_static/releases/{}/web_static'
             .format(file_name))
         run('rm -rf /data/web_static/current')
-        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'
+        run('ln -s /data/web_static/releases/{} /data/web_static/current'
             .format(file_name))
         return True
     except Exception as e:
