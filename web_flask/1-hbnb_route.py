76e965fa5a6bb7833dac6054b9cc114e334f0dd9
[esekyi] <[sskert10@gmail.com]>
2024-07-29 04:39:36 +0000

    1. HBNB

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
 
-"""0. Hello Flask - Flask web framework"""
+"""1. HBNB - Flask web framework"""
 
 from flask import Flask
 
 app = Flask(__name__)
+
 
 @app.route("/", strict_slashes=False)
 def index():
 	"""A route function"""
 	return "Hello HBNB!"
 
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+	"""A route function that displays HBNB"""
+	return "HBNB"
 
 if __name__ == "__main__":
-	app.run(port=5000, host= "0.0.0.0")
+	app.run(port=5000, host="0.0.0.0")
